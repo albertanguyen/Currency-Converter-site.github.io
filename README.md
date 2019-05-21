67b5400264866743eee7bc373b5d9e859d0440a5
albertanguyen <jaynguyen2@outlook.com>
2019-05-22 02:24:16 +0700

    Update README, thanks messages, currency bill computation

--- README.md
+++ README.md
@@ -1,6 +1,62 @@
-# Currency Converter website
+# CoderSchool FTW - Currency Converter website
 
-* Using css, html, Java Script
-* Homepage:
+Created with love by: Anh Nguyen
+  
+View online at: https://albertanguyen.github.io/Currency-Converter-site.github.io/
+  
+Using css, html, Java Script to create a simple site of Currency Converter.
+
+## Homepage:
 
 <img src="https://github.com/albertanguyen/Currency-Converter-site.github.io/blob/master/img/homepage.png" width="800" align="center">
+
+[//]: # (## Video Walkthrough)
+
+[//]: # (Here's a walkthrough of implemented user stories.)
+
+
+## User Stories
+
+The following **required** functionalities are completed (this is a sample):
+
+* [x] The user can choose any currency in this list: VND, USD, KRW, IDR to convert between each other. 
+* [x] The user can press a button Convert to start the calculation. 
+* [x] After the user click on Convert, the result is displayed in the Converted Value box. 
+* [ ] The user sees number of bills of the currency that they want to convert to convert to in correponding with the converted amount
+
+[//]: # (The following **additional** features are implemented:)
+
+[//]: # (* [x] List anything else that you can get done to improve the page!)
+
+[//]: # (## Time Spent and Lessons Learned)
+
+[//]: # (Time spent: **X** hours spent in total.)
+
+[//]: # (Describe any challenges encountered while building the app.)
+
+## Version 1.0.0
+Adapted from the live demo of Mr. Loi
+
+## License
+
+    Copyright [2019] [Anh Nguyen]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+
+
+
+
+
+
+
--- js/script.js
+++ js/script.js
@@ -71,18 +71,95 @@ function addFormattingToMoney(amount) {
   return amount.toFixed(4).replace(/\d(?=(\d{3})+\.)/g, "$&,");
 }
 
+/* Compute number of bills*/
 function VNDBillsComputation(amount) {
+  let NumberBill = [];
   let Bill500k = amount % 500000;
   let Bill200k = Bill500k % 200000;
   let Bill100k = Bill200k % 100000;
   let Bill50k = Bill100k % 50000;
   let Bill20k = Bill50k % 20000;
-  let Bill10k = Bill10k % 10000;
-  let Bill5k = Bill5k % 5000;
-  let Bill2k = Bill2k % 2000;
-  let Bill1k = Bill1k % 1000;
+  let Bill10k = Bill20k % 10000;
+  let Bill5k = Bill10k % 5000;
+  let Bill2k = Bill5k % 2000;
+  let Bill1k = Bill2k % 1000;
+  NumberBill.push(Bill500k);
+  NumberBill.push(Bill200k);
+  NumberBill.push(Bill100k);
+  NumberBill.push(Bill50k);
+  NumberBill.push(Bill20k);
+  NumberBill.push(Bill10k);
+  NumberBill.push(Bill5k);
+  NumberBill.push(Bill2k);
+  NumberBill.push(Bill1k);
+  return NumberBill;
 }
 
+function ThanksMessage(firstCurrency, secondCurrency) {
+    const formattedCurrency = secondCurrency.toLowerCase();
+    if (firstCurrency === "vnd") {
+    switch (formattedCurrency) {
+      case "usd":
+        return "Thank you";
+        break;
+      case "krw":
+        return "고맙습니다";
+        break;
+      case "idr":
+        return "Terima kasih";
+    }
+  }
+  if (firstCurrency === "usd") {
+    switch (formattedCurrency) {
+      case "vnd":
+        return "Cám ơn";
+        break;
+      case "krw":
+        return "고맙습니다";
+        break;
+      case "idr":
+        return "Terima kasih";
+    }
+  }
+  if (firstCurrency === "krw") {
+    switch (formattedCurrency) {
+      case "vnd":
+        return "Cám ơn";
+        break;
+      case "usd":
+        return ;
+        break;
+      case "idr":
+        return "Terima kasih";
+    }
+  } else if (firstCurrency === "idr") {
+    switch (formattedCurrency) {
+      case "vnd":
+        return "Cám ơn";
+        break;
+      case "usd":
+        return "Thank you";
+        break;
+      case "krw":
+        return "고맙습니다";
+    }
+  }
+}
+
+function displayBillsVND(amount) {
+  const convertedValue = amount;
+  const NumOfBill = VNDBillsComputation(convertedValue);
+  return [` 500,000 VND x ${NumOfBill[0]}\n`,
+          `200,000 VND x ${NumOfBill[1]}\n`,
+          `100,000 VND x ${NumOfBill[2]}\n`,
+          `50,000 VND x ${NumOfBill[3]}\n`,
+          `20,000 VND x ${NumOfBill[4]}\n`,
+          `10,000 VND x ${NumOfBill[5]}\n`,
+          `5,000 VND x ${NumOfBill[6]}\n`,
+          `2,000 VND x ${NumOfBill[7]}\n`,
+          `1,000 VND x ${NumOfBill[8]}\n`];
+
+}
 
 function ExecutionCurrencyConverter() {
   const valueOfInput = document.getElementById("amount").value;
@@ -96,7 +173,14 @@ function ExecutionCurrencyConverter() {
   );
 
   document.getElementById("result").value = convertedValue;
+  let mess = ThanksMessage(firstCurrency, secondCurrency);
+  document.getElementById("Thankyou").innerText = mess;
+  let bills = displayBillsVND(convertedValue);
+  document.getElementById("NumberOfBills").innerText = bills[0]
+
+
 }
+
 
 function callApi(currency) {
   var xhr = new XMLHttpRequest();
